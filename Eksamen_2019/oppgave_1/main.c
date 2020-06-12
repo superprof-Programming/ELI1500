5a01dc83d211ca73e8e7ca5aa85ba2ad4b1c5bb1
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-06-12 11:53:19 +0200

    update oppgave 1#

--- Eksamen_2019/oppgave_1/main.c
+++ Eksamen_2019/oppgave_1/main.c
@@ -3,23 +3,43 @@
 
 int main() {
 
-    float kap = 0.10;
+
+    float kap = 1.10;
     int prisPrPakke = 734;
-    float AntallKvadratMeterPrGulv;
+    float KvadratMeter;
     printf("Skriv inn antall kvadratmeter: ");
-    scanf("%f", &AntallKvadratMeterPrGulv);
+    scanf("%f", &KvadratMeter);
 
+    float antallPakerSomTrengs = ((KvadratMeter * kap)  / 2.952);
 
-    // Beregner pris per pakke
-    float TiprosentOkning = prisPrPakke * kap;
+    float totalPris = (prisPrPakke * antallPakerSomTrengs);
 
-    int antallPakerSomTrengs = floor(AntallKvadratMeterPrGulv / 2952);
+    if (antallPakerSomTrengs > 6.12) {
+        antallPakerSomTrengs = floor(antallPakerSomTrengs + 0.5);
+        printf("Det er da beregnet noe under 10%% kap\n");
+    }
 
-    float totalPris = (prisPrPakke * antallPakerSomTrengs) + TiprosentOkning;
+    printf("\nAnbefaler %d pakker.\n", antallPakerSomTrengs);
+    printf("\nTOTAL PRIS: %.2f kr.\n", totalPris);
 
-    printf("Det er beregnet under 10% kapp");
-    printf("\nAnbefaler %d pakka", antallPakerSomTrengs);
-    printf("\nTOTAL PRIS: %.2f", totalPris);
+//
+//    float kap = 0.10;
+//    int prisPrPakke = 734;
+//    float AntallKvadratMeterPrGulv;
+//    printf("Skriv inn antall kvadratmeter: ");
+//    scanf("%f", &AntallKvadratMeterPrGulv);
+//
+//
+//    // Beregner pris per pakke
+//    float TiprosentOkning = prisPrPakke * kap;
+//
+//    int antallPakerSomTrengs = floor(AntallKvadratMeterPrGulv / 2952);
+//
+//    float totalPris = (prisPrPakke * antallPakerSomTrengs) + TiprosentOkning;
+//
+//    printf("Det er beregnet under 10% kapp");
+//    printf("\nAnbefaler %d pakka", antallPakerSomTrengs);
+//    printf("\nTOTAL PRIS: %.2f", totalPris);
 
     return 0;
 }

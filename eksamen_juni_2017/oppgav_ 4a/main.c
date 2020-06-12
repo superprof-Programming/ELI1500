05bfbeb64b7e63fda8cbf5c4b2b8af65a5a69860
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-06-12 04:19:12 +0200

    start - oppgave 4a

---
+++ eksamen_juni_2017/oppgav_ 4a/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+    Kvisthullfabrikken i Grukkedalen produserer store, middels og små kvisthull. De lagrer data
+    over produksjonen hver uke i en fil på formatet:
+
+    Produksjon år 2017
+
+    Uke1 54 store 103 middels 27 små
+            Uke2 67 store 210 middels 89 små
+    ……. osv
+            Lag et program som åpner fila «produksjon2017.txt», og summerer og skriver ut antall
+    store, middels og små kvisthull som er produsert hittil i år. Programmet skal virke uansett
+    hvilken uke det kjøres, så programmet må lese fila helt til den er slutt.
+ */
+int main() {
+    
+    return 0;
+}
